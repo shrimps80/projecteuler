b6a6ed25c92075731cc81174c46840cb9e0a3fad
shrimp <shrimps80@gmail.com>
2018-01-23 15:52:14 +0800

    err

---
+++ 007.php
@@ -0,0 +1,27 @@
+<?php
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?
+*/
+// $n = 1;
+// $i = 1;
+// $num = 6;
+// while (true) {
+//     $k = 0;
+//     for ($j = 1; $j < $i; $j++) {
+//         if ($i % $j == 0) {
+//             $k++;
+//         }
+//     }
+//     if ($k == 1) {
+//       if ($n == $num)
+//       {
+//         echo $i . "\n";
+//         break;
+//       }
+//       $n++;
+//     }
+//     $i++;
+// }
+?>
